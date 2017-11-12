f4e44f2c29ef18db7fe396a8ad80382f8724af38
Minita Dabhi <dabhi.minita@gmail.com>
2017-11-12 18:26:14 -0500

    Adding file SymbolPyramid.py

---
+++ SymbolPyramid.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+"""
+///HackerRank///
+Problem : Write a program that prints a staircase of size n.
+@ input: size n
+e.g. 5
+     @ output: Prints a staircase of size n
+e.g.
+      *
+     **
+    ***
+   ****
+  *****
+
+"""
+import sys
+
+n = int(input().strip())
+i = 1
+j = n-1
+
+for i in range(1,n+1,1):
+    print(' '*j + '*'*i)
+    j = j-1
